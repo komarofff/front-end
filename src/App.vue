1e1797afd06272a95025f19bb1e5f6043320d580
Anatoliy Komarov <admin@komaroff.biz>
2021-09-25 09:00:44 +0300

    2

---
+++ src/App.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h2>Portfolio links</h2>
+      <ol>
+        <li v-for="item in frontend">
+          <a target="_blank" :href="item.link">{{ item.linkText }}</a> <span v-if="item.description.length>0">- {{ item.description }}</span>
+           <a class="source-code" v-if="item.source.length>0 " target="_blank" :href="item.source"> → source code </a>
+        </li>
+      </ol>
+      <hr>
+      <h2>Vue.js</h2>
+      <ol>
+        <li v-for="item in vueJS">
+          <a target="_blank" :href="item.link">{{ item.linkText }}</a> <span v-if="item.description.length>0">- {{ item.description }}</span>
+          <a  class="source-code" v-if="item.source.length>0 " target="_blank" :href="item.source"> → source code </a>
+        </li>
+      </ol>
+      <hr>
+      <h2>JavaScript</h2>
+      <ol>
+        <li v-for="item in java">
+          <a target="_blank" :href="item.link">{{ item.linkText }}</a> <span v-if="item.description.length>0">- {{ item.description }}</span>
+          <a  class="source-code" v-if="item.source.length>0 " target="_blank" :href="item.source"> → source code </a>
+        </li>
+      </ol>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      frontend: [
+        {link: 'https://komarofff.github.io/Calendar/Calendar_week.html', linkText: 'Admin panel (calendar). in progress...', source: 'https://github.com/komarofff/Calendar',
+          description: ' TailWind + JavaScript + drag&&drop popup (desktop version)'},
+        {link: 'https://komarofff.github.io/upwork/index.html', linkText: 'Admin panel ', source: 'https://github.com/komarofff/upwork', description: 'TailWind + JavaScript (desktop version)'},
+        {link: 'https://komarofff.github.io/page5/index.html',
+          linkText: 'Landing page.', source: 'https://github.com/komarofff/page5', description: ' TailWind + JavaScript + SlickSlider'},
+        {link: 'https://komarofff.github.io/page4/index.html', linkText: 'Multi pages  ', source: 'https://github.com/komarofff/a1', description: 'Laravel+Vue.js+GRID+FLEX+JavaScript+SlickSlider'},
+        {link: 'https://1c-web.com',
+          linkText: 'https://1c-web.com', source: 'https://github.com/komarofff/1c-web', description: 'Multi pages (Laravel+Vue.js+GRID+FLEX+JavaScript+SlickSlider)'},
+        {link: 'https://komarofff.github.io/page2/index.html', linkText: 'Internet shop ', source: 'https://github.com/komarofff/page2', description: ' HTML5,CSS3,JavaScript+slider'},
+        {link: 'https://komarofff.github.io/page1/index.html', linkText: 'Landing page (trip to Japan)', source: 'https://github.com/komarofff/page1', description: 'HTML5,CSS3,JavaScript'},
+        {link: 'https://vasterra.com/blog', linkText: 'vasterra.com/blog', source: '', description: 'Blog.Wordpress'},
+        {link: 'https://vasterra.com/', linkText: 'vasterra.com', source: '', description: 'index page. Laravel+JavaScript+SlickSlider+get data from Wordpress'},
+        {link: 'https://vasterra.com/mobile_experiences', linkText: 'vasterra.com/mobile_experiences', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/software_for_startups', linkText: 'vasterra.com/software_for_startups', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/prema', linkText: 'vasterra.com/prema', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/yablochkov', linkText: 'vasterra.com/yablochkov', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/edu', linkText: 'vasterra.com/edu', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/docNow', linkText: 'vasterra.com/docNow', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/finGo', linkText: 'vasterra.com/finGo', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/lms_development', linkText: 'vasterra.com/lms_development', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/dfc', linkText: 'vasterra.com/dfc', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://vasterra.com/graam', linkText: 'vasterra.com/graam', source: '', description: 'Landing. Laravel+JavaScript+SlickSlider+Vue.js'},
+        {link: 'https://otdyh-v-italyi.ru/', linkText: 'otdyh-v-italyi.ru', source: '', description: 'Wordpress'},
+        {link: 'https://1c-group.by', linkText: '1c-group.by', source: '', description: 'Modx, PHP,HTML,JavaScript,jQuery'},
+        {link: 'https://1c-po.by', linkText: '1c-po.by', source: '', description: 'Modx, PHP,HTML,JavaScript,jQuery'},
+        {link: 'https://1c-buh.by', linkText: '1c-buh.by', source: '', description: 'Modx, PHP,HTML,JavaScript,jQuery'},
+        {link: 'https://svetelektro.by', linkText: 'svetelektro.by', source: '', description: 'Internet shop. PHP,HTML5,JavaScript,jQuery+Admin panel'},
+        {link: 'https://tdm-electric.by', linkText: 'tdm-electric.by', source: '', description: 'Internet shop. PHP,HTML5,JavaScript,jQuery + Admin panel + integration with the accounting program  '},
+
+      ],
+      java: [
+        {link: 'https://komarofff.github.io/karusel/karusel-levo-pravo.html', linkText: 'Carousel1 ', source: 'https://github.com/komarofff/page1', description: 'JS left-right-drag&drop'},
+        {link: 'https://komarofff.github.io/karusel/karusel-po-krugu.html', linkText: 'Carousel2 ', source: 'https://github.com/komarofff/karusel/karusel-po-krugu.html', description: 'JS left-right-drag&drop-in a circle'},
+        {link: 'https://komarofff.github.io/karusel/slider-without-stop.html', linkText: 'Slider', source: 'https://github.com/komarofff/karusel/blob/main/slider-without-stop.html', description: 'slider-without-stop( left-to-right)'},
+        {link: 'https://komarofff.github.io/karusel/svg-animation.html', linkText: 'SVG animation', source: 'https://github.com/komarofff/karusel/svg-animation.html', description: 'svg-animation ( fill circle)'},
+        {link: 'https://komarofff.github.io/marafon-JS/slider-cars/index.html', linkText: 'Slider-flex', source: 'https://github.com/komarofff/marafon-JS/tree/main/slider-cars', description: 'Slider based on flex grow and flex shrink +JS'},
+        {link: 'https://komarofff.github.io/marafon-JS/slider-up-down/', linkText: 'Slider up-to-down', source: 'https://github.com/komarofff/marafon-JS/tree/main/slider-up-down',
+          description: 'Slider up-down . 1 page, screen size'},
+        {link: 'https://komarofff.github.io/marafon-JS/drag-and-drop/index.html', linkText: 'drag&drop', source: 'https://github.com/komarofff/marafon-JS/tree/main/drag-and-drop', description: 'drag&drop'},
+
+      ],
+      vueJS: [
+        {link: 'https://komaroff.biz/vue/babka/', linkText: 'Jam to jars ', source: 'https://github.com/komarofff/Vue.js/tree/main/babka', description: 'Vue.js,vuex,vue-router'},
+        {link: 'https://komarofff.github.io/todoList/index.html', linkText: 'TODO list', source: 'https://github.com/komarofff/todoList', description: ''},
+        {link: 'https://komarofff.github.io/dealList/', linkText: 'Deal list', source: 'https://github.com/komarofff/dealList', description: ''},
+
+      ]
+    }
+  }
+
+}
+</script>
+
+<style>
+body {
+  background: #454a56;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+
+}
+
+.container {
+  width: 98vw;
+  margin:0 auto;
+  background: #454a56;
+  overflow: hidden;
+}
+.card{
+  max-width: 1000px;
+  background: #fff;
+  border-radius: 20px;
+  padding:20px;
+  margin:0 auto;
+  height: 80vh;
+  overflow: hidden;
+  overflow-y: auto;
+
+}
+.card::-webkit-scrollbar{
+  width: 5px;
+}
+
+.card::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px #454a56;
+}
+
+.card::-webkit-scrollbar-thumb {
+  background-color: #454a56;
+}
+li{
+  margin:10px 0;
+}
+a{
+  color: #0d0dfa;
+  text-decoration: none;
+  padding-bottom:2px;
+}
+a:hover{
+  border-bottom:2px solid #0d0dfa;
+}
+.source-code{
+  text-decoration: none;
+  color: #8c0505;
+  font-weight: bold;
+
+}
+</style>
